0d4f295c4a20b38e0875a104696a4580b568c2b5
s-meena074 <132200346+s-meena074@users.noreply.github.com>
2023-04-30 08:03:09 -0400

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class PrimeNumber 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int n= 29;
+		if(n%2==0)
+		{
+			System.out.println("Prime Number" +n); 
+		}
+		else if (n%3 == 0)
+		{System.out.println("Prime Number" +n); }
+		else System.out.println("Non Prime Number");
+	}
+
+}
