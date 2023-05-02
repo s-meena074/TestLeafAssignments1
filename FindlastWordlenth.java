b74023a26979fb0560123caf5321ccd2c3de824b
s-meena074 <132200346+s-meena074@users.noreply.github.com>
2023-05-02 10:24:53 -0400

    Add files via upload

---
+++ FindlastWordlenth.java
@@ -0,0 +1,14 @@
+package PracticeJava;
+
+public class FindlastWordlenth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String word = "luffy is still joyboy"; 
+		String []xy = word.split(" "); 
+		System.out.println(xy[xy.length-1].length());
+
+	}
+
+}
