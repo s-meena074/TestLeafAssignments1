c5b406853fa04264b69abd8b3882091d79cb565f
s-meena074 <132200346+s-meena074@users.noreply.github.com>
2023-05-03 09:23:47 -0400

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 8;
+		System.out.println("Input:" +a); 
+		double b = Math.sqrt(a);
+		System.out.println("Output:"+String.format("%.1f",b));
+			
+
+	}
+
+}
